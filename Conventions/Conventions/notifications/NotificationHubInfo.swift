8987ecc82a6485c93325ac57f533f70753e98623
dbahat <dbahat@live.com>
2017-08-30 21:18:27 +0300

    Updated notification hub settings for icon 2017

--- Conventions/Conventions/notifications/NotificationHubInfo.swift
+++ Conventions/Conventions/notifications/NotificationHubInfo.swift
@@ -9,13 +9,13 @@
 import Foundation
 
 class NotificationHubInfo {
-    static let NAME = "conventions"
-    static let CONNECTIONSTRING = "Endpoint=sb://sff-conventions.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=dCrJSDNwpotcGHGwLo4EKObl8cAtVVagaUO3CtPuMSo="
+    static let NAME = "icon2017"
+    static let CONNECTIONSTRING = "Endpoint=sb://sff-conventions.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=iQiBIOy0yZJJIuUIpOtpzmd6O5AQQdmZGtoN3J4TXH0="
     
-    static let CATEGORY_GENERAL = "icon2016_general"
-    static let CATEGORY_EVENTS = "icon2016_events"
-    static let CATEGORY_EMERGENCY = "icon2016_emergency"
-    static let CATEGORY_TEST = "icon2016_test"
+    static let CATEGORY_GENERAL = "icon2017_general"
+    static let CATEGORY_EVENTS = "icon2017_events"
+    static let CATEGORY_EMERGENCY = "icon2017_emergency"
+    static let CATEGORY_TEST = "icon2017_test"
     
     static let DEFAULT_CATEGORIES = [CATEGORY_GENERAL, CATEGORY_EVENTS, CATEGORY_EMERGENCY]
 }
